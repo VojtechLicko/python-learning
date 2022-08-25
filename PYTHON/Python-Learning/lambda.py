7a84604e71bb13e560608b7d1155301e43f4a04f
Vojtěch Ličko <57452806+Q3NYX@users.noreply.github.com>
2022-08-25 10:44:04 +0200

    OOP, Tictactoe, lambda

--- PYTHON/Python-Learning/lambda.py
+++ PYTHON/Python-Learning/lambda.py
@@ -1,15 +1,9 @@
-var = lambda x, y: x + y
-print(var(60, 88))
+
+y = (lambda x, y: x+y)
+print(y(60, 88))
 
 
-def addition(x, y):
-    res = (x + y)
-    return res
-
-
-print(addition(7, 8))
+x = (lambda x, y: x*y)
+print(x(5, 6))
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression.
-
-x = lambda a, b, c: a + b + c
-print(x(5, 6, 2))
