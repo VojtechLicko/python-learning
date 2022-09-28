48d1b4eda85eacf4df7fef4168c81eba8794eaca
Vojtěch Ličko <57452806+Q3NYX@users.noreply.github.com>
2022-09-28 18:00:21 +0200

    Added comments with subprolems to the top of the file

--- PYTHON/Chess_Project/main.py
+++ PYTHON/Chess_Project/main.py
@@ -3,6 +3,23 @@ import pygame
 from chess_board import create_chessboard
 from chess_pieces import Pieces
 
+# Sub problems:
+# Create board
+# Be able to know coords of squares on board
+# Place pieces on the board (initial position)
+# Be able to move the pieces around to squares
+# Check what kind of piece it is on click eg. queen pawn king
+# Check if the piece is left clicked and select it >> chage the color
+# Create Logic for pieces
+# If dont move the piece dont count it as move
+# Highlight squares the piece can move to from their current position
+# Change turns
+# Keep track of current pieces on the board or the captured pieces
+# Cant move something that if i move gets me in check
+# See if i am checked
+# Check for winner
+# Menu > be able to reset board
+
 WIDTH, HEIGHT = 800, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Chess Game")
